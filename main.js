bee4cd9873fecd0988e0c082172847784bfb51ba
Ramon Tabilin <tabi0011@algonquinlive.com>
2017-10-16 01:05:31 -0400

    Second upload
    Added wage input.

--- main.js
+++ main.js
@@ -1,21 +1,22 @@
 document.addEventListener("DOMContentLoaded", init); 
 
-const wage = 11.75;
+//const wage = 11.75;
 const taxShit = 0.824;
 
 function init() {
     console.log('Initiated!');
     let income = document.getElementById("income");
     document.getElementById("calculate").addEventListener("click", function () {
+        var wage = document.getElementById("wage").value;
         var hours = document.getElementById("hours").value;
 //        console.log(hours);
-        console.log(calc(hours));
+        console.log(calc(hours, wage));
          
-        income.textContent = "Estimated income: $" + calc(hours);
+        income.textContent = "Estimated income: $" + calc(hours, wage);
     });
 }
 
-function calc(hours) {
+function calc(hours, wage) {
     let sum = (hours * wage) * taxShit;
     let total = parseInt(sum);
     return total;
